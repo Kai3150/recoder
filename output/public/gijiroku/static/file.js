c9f1d1cde7814bf5d27c7953368057bb4b5cba9c
kai <hurukawasiro3150@gmail.com>
2022-09-15 21:14:57 +0900

    for heroku

---
+++ output/public/gijiroku/static/file.js
@@ -0,0 +1,157 @@
+const dropZone = document.getElementById('drop-zone');
+const preview = document.getElementById('preview');
+const uploaded = document.getElementById('uploaded');
+
+const fileInput = document.getElementById('file-input');
+//formDomを追加する。
+const formDOM = document.querySelector(".form-img");
+
+let imgName;
+const htmlName = window.location.href.split('/').pop();
+let imgType;
+
+
+dropZone.addEventListener('dragover', function (e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.style.background = '#e1e7f0';
+}, false);
+
+dropZone.addEventListener('dragleave', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    this.style.background = '#ffffff';
+    console.log('in the leave');
+}, false);
+
+fileInput.addEventListener('change', function () {
+    console.log('in  the change');
+    e.preventDefault();
+    previewFile(this.files[0]);
+    console.log(files);
+});
+
+dropZone.addEventListener('drop', function(e) {
+    e.stopPropagation();
+    e.preventDefault();
+    //this.style.background = '#ffffff'; //背景色を白に戻す
+    let files = e.dataTransfer.files; //ドロップしたファイルを取得
+    //console.log(files);
+    preview.innerHTML = '';
+
+    //if (files.length > 1) return alert('アップロードできるファイルは1つだけです。');
+    fileInput.files = files; //inputのvalueをドラッグしたファイルに置き換える。
+    const len = 100 / files.length;
+
+    for (let i = 0; i < files.length; i++) {
+        const file = files[i];
+
+        //previewFile(file)//の置き換え
+        let fr = new FileReader();
+        fr.readAsDataURL(file);
+        fr.onload = function () {
+            let img = document.createElement('img');
+            img.setAttribute('src', fr.result);
+            img.style.width = `${len}%`;
+            preview.appendChild(img);
+        };
+        //previewFile終わり
+
+        imgName = file.name;
+        imgType = "bansho";
+    }
+}, false);
+
+function previewFile(file) {
+    /* FileReaderで読み込み、プレビュー画像を表示。 */
+    let fr = new FileReader();
+    fr.readAsDataURL(file);
+    fr.onload = function() {
+        let img = document.createElement('img');
+        img.setAttribute('src', fr.result);
+        img.style.width = ``
+        preview.appendChild(img);
+    };
+}
+
+// htmlファイルの名前 -> そのファイルのimgの名前
+const getImages = async () => {
+    try {
+        let imgs = await axios.get("/api/v1/imgs", {
+            params: {
+                // ファイル名をつける
+                //htmlName: 'sample.html'
+                htmlName: window.location.href.split('/').pop()
+            }
+        });
+
+
+        let { data } = imgs;
+        //出力
+        imgs = data.map((img) => {
+                const { htmlName, imgName, imgType } = img;
+                return `<img src="/uploads/${imgName}" >`;
+            })
+            .join("");
+        //挿入
+        preview.innerHTML = imgs;
+    } catch (err) {
+        console.log(err);
+    }
+};
+
+const getImages2 = async () => {
+    const action = formDOM.getAttribute("action")
+    const options = {
+        method: 'GET',
+        headers: new Headers({"htmlname": htmlName,})
+    }
+
+    fetch("/api/v1/imgs", options)
+    .then(imgs => {
+        return imgs.json();
+    }).then(post => {
+        //出力
+        const len = 100 / post.length;
+        uploaded.innerHTML = '';
+        imgs = post.map((img) => {
+            const { htmlName, imgName, imgType } = img;
+            const imgel = document.createElement('img');
+            imgel.setAttribute('src', `/uploads/${imgName}`);
+            imgel.style.width = `${len}%`;
+            uploaded.appendChild(imgel);
+        });
+    });
+};
+
+getImages2()
+
+formDOM.addEventListener("submit", async (e) => {
+    e.preventDefault();
+
+    const files = fileInput.files;
+    const formData = new FormData();
+    for (let i = 0; i < files.length; i++) {
+        formData.append('files', files[i]);
+    }
+
+    const action = formDOM.getAttribute("action")
+    const options = {
+        method: 'POST',
+        headers: new Headers({
+            "htmlname": htmlName,
+            "imgname": imgName,
+            "imgtype": imgType
+        }),
+        body: formData,
+    }
+    fetch(action, options).then((e) => {
+        if (e.status === 200) {
+            alert("保存しました。")
+            return
+        }
+        alert("保存できませんでした。")
+    })
+    preview.innerHTML = "";
+    getImages2();
+});
---
+++ server.js
@@ -0,0 +1,59 @@
+const express = require('express');
+const path = require('path');
+const multer = require('multer');
+const mongoose = require("mongoose");
+const app = express();
+const Thread = require("./output/models/Thread");
+
+app.use(express.json());
+app.use(express.static(path.join(__dirname, 'output/public')));
+
+mongoose.connect("mongodb+srv://kai:Kkkh3150@cluster0.4mb3bi1.mongodb.net/?retryWrites=true&w=majority")
+    .then(() => console.log('DB connected'))
+    .catch((err) => console.log(err));
+
+
+
+// get imgメソッド
+app.get("/api/v1/imgs", async (req, res) => {
+    try {
+        const imgs = await Thread.find({htmlName: req.headers.htmlname});
+        res.status(200).json(imgs);
+    } catch (error) {
+        console.log(error);
+    }
+})
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        cb(null, 'output/public/uploads/');
+    },
+    filename: function (req, file, cb) {
+        cb(null, file.originalname);
+    }
+})
+const upload = multer({ storage: storage });
+
+//post メソッド
+app.post('/upload', upload.array('files'), async (req, res, next) => {
+    let search = [];
+    req.files.forEach(file => {
+        search.push({
+            htmlName: req.headers.htmlname,
+            imgName: file.originalname,
+            imgType: req.headers.imgtype
+        })
+    });
+    try {
+        const createThread = await Thread.create(search);
+        res.status(200).json(createThread);
+        console.log('new create');
+    } catch (error) {
+        res.status(200).json({});
+        console.log(error);
+    }
+})
+
+let server = app.listen(3000, function () {
+    console.log("listening at port %s", server.address().port);
+});
